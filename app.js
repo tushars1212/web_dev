567a86c889eb14c4aa88cfab830ba8c0ad957abe
tushars1212 <tusharsatija9099@gmail.com>
2023-07-26 14:02:18 +0530

    calculator project

---
+++ app.js
@@ -0,0 +1,27 @@
+const keypad = document.getElementById('keypad');
+const screen = document.getElementById('display');
+
+keypad.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'BUTTON') {
+       
+        const buttonText = event.target.innerText;
+
+        if (buttonText === 'C') {
+            screen.value = "";
+        }
+        else if (buttonText === 'x') {
+            screen.value += "*";
+        }
+        else if (buttonText === '=') {
+            try {
+                screen.value=eval(screen.value);
+            }
+            catch (e) {
+                screen.value = "Invalid Operation";
+            }
+        }
+        else {
+            screen.value += buttonText;
+        }  
+    }
+});
